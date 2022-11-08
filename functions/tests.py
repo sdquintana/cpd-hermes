eb3ec36c5a246aa27703f7bef169b1b42c62807c
rmaxu <uhrmax93@hotmail.com>
2022-11-07 22:40:20 -0600

    test function and endpoint

--- functions/tests.py
+++ functions/tests.py
@@ -22,14 +22,16 @@ def create_event(event, context):
         checker = Checker(expected_schema)
         checker.validate(event_in)
     except Exception as e:
-
+        print(e)
+        body = {
+            "success": False,
+            "message": f"{e}"
+        }
         response = {
             "statusCode": 400,
-            "body": f"{e}"
+            "body": json.dumps(body)
         }
         return response
-
-    print(event_in, type(event_in))
 
     s3 = boto3.client('s3')
 
@@ -38,36 +40,28 @@ def create_event(event, context):
     ip = event['requestContext']['identity']['sourceIp']
 
     json_object = {"id": id,
-                   'user_id': event_in.get('user_id'),
-                   'funnel': event_in.get('funnel'),
-                   'type': event_in.get('type'),
-                   'event': event_in.get('event'),
-                   'traits': json.dumps(event_in.get('traits')),
-                   'timestamp': f"{datetime.today()}",
+                   "user_id": event_in.get('user_id'),
+                   "event": event_in.get('event'),
+                   'traits': event_in.get('traits'),
+                   'original_timestamp': f"{datetime.today()}",
+                   'type': 'track',
                    'properties': json.dumps(event_in.get('properties')),
-                   'channel': event_in.get('channel'),
                    'ip': str(ip)
-
                    }
-
     s3.put_object(
         Body=json.dumps(json_object),
         Bucket='reingeniate-data-raw',
-        Key=f'events/{date_storage}/{id}.json'
+        Key=f'storyplace/events/{date_storage}/{id}.json'
     )
 
-    response = {
-        "statusCode": 200,
-        "body": f"{json_object}"
+    body = {
+        "success": True,
+        "message": json_object
     }
 
-    return response
-
-
-def get_event(event, context):
     response = {
         "statusCode": 200,
-        "body": f"{event}"
+        "body": json.dumps(body)
     }
 
     return response
